80acfd87a6662f3f9f82261fb861c5fbfa4a1135
Dawid Osuchowski <dwdosuchowski@gmail.com>
2020-07-05 17:35:45 +0200

    Initial commit

---
+++ Assets/Scripts/BodyScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class BodyScript : MonoBehaviour
+{
+    public new Camera camera;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("GroundBlock"))
+        {
+            VideoPlayer videoPlayer = other.gameObject.GetComponent<VideoPlayer>();
+            if (videoPlayer == null)
+                return;
+            var foundVideoPlayers = FindObjectsOfType<VideoPlayer>();
+            foreach (var foundVidPlayer in foundVideoPlayers)
+            {
+                if (foundVidPlayer != videoPlayer && foundVidPlayer.targetCamera != null)
+                {
+                    videoPlayer.Play();
+                    videoPlayer.targetCamera = camera;
+                    foundVidPlayer.targetCamera = null;
+                    foundVidPlayer.Stop();
+                }
+            }
+        }
+    }
+}
